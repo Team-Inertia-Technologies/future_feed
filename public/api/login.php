ada9026bad79a624510577185d7d68b04479d929
Myron <myron@teaminertia.com>
2026-02-09 15:09:40 +0530

    changed
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/login.php
+++ public/api/login.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $NO_REDIRECT = $NO_PRELOAD = 1;
 include "../includes/common_api.php";
-require_once "../../vendor/autoload.php";
+require_once "../vendor/autoload.php";
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
