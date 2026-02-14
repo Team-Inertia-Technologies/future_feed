821687a8f7e753a31cdb554b24681e7e1dfcea9b
Myron14 <pintomyron14@gmail.com>
2026-02-14 10:49:44 +0530

    modified

---
+++ public/api/video_watched.php
@@ -0,0 +1,56 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+$NO_REDIRECT = $NO_PRELOAD = 1;
+include "../includes/common_api.php";
+header('Content-Type: application/json');
+$postdata = file_get_contents("php://input");
+
+$request = json_decode($postdata, true);
+$_REQUEST = array_merge($_REQUEST, $request ?? []);
+
+$token = $_REQUEST['token'] ?? '';
+$videoid = $_REQUEST['iVideoID'] ?? '';
+$duration = $_REQUEST['duration'] ?? '';
+
+if (!$token) {
+    http_response_code(400);
+    header('Content-Type: application/json');
+    echo json_encode([
+        "statusCode" => 400,
+        "error" => [
+            "message" => "Missing token"
+        ]
+    ]);
+    exit;
+}
+$userid = DecodeParam($token);
+
+try {
+    $id = NextID('iAssocID', 'user_watched_video');
+    $now = NOW;
+    $query = "INSERT INTO user_watched_video (iAssocID, iUserID, iVideoID, vDuration, dtAdded) VALUES ($id, $userid, $videoid, $duration, '$now')";
+    sql_query($query);
+
+    $response = array(
+        "statusCode" => 200,
+        "data" => array(
+            "message" => "Video added successfully"
+        )
+    );
+    http_response_code(200);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    exit;
+} catch (Exception $e) {
+    $response = array(
+        "error" => array(
+            "message" => $e->getMessage()
+        ),
+        "statusCode" => 500,
+    );
+    http_response_code(500);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    exit;
+}
