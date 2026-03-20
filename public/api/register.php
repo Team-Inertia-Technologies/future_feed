ff976b3c3193dbf3af941d1f6fd79ea03be334a2
Myron <myron@teaminertia.com>
2026-03-20 11:40:42 +0530

    modified
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/register.php
+++ public/api/register.php
@@ -3,6 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 $NO_REDIRECT = $NO_PRELOAD = 1;
 include "../includes/common_api.php";
+require_once "../vendor/autoload.php";
 use Firebase\JWT\JWT;
 use Firebase\JWT\JWK;
 header('Content-Type: application/json');
