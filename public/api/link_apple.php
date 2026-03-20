4d1ffac32b67123dd397e61c3bf232bc4203e55d
Myron <myron@teaminertia.com>
2026-03-20 10:51:32 +0530

    MODIFIED
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/link_apple.php
+++ public/api/link_apple.php
@@ -9,36 +9,52 @@ $postdata = file_get_contents("php://input");
 $request = json_decode($postdata, true);
 $_REQUEST = array_merge($_REQUEST, $request ?? []);
 
-$phone    = $request['phone'];
-$apple_id = $request['apple_id'];
-$q = "SELECT iUserID FROM user 
-      WHERE vMobile = '" . db_input($phone) . "' 
-      AND cStatus = 'A' LIMIT 1";
+$phone    = db_input($request['phone'] ?? '');
+$apple_id = db_input($request['apple_id'] ?? '');
+
+if (empty($phone) || empty($apple_id)) {
+    http_response_code(400);
+    echo json_encode([
+        "statusCode" => 400,
+        "error" => ["message" => "Phone and Apple ID are required"]
+    ]);
+    exit;
+}
+
+$checkApple = sql_query("SELECT iUserID FROM user WHERE vAppleID = '$apple_id' LIMIT 1", 'LINK.APPLE.CHECK');
+
+if (sql_num_rows($checkApple)) {
+    http_response_code(409);
+    echo json_encode([
+        "statusCode" => 409,
+        "error" => ["message" => "This Apple ID is already linked to another account"]
+    ]);
+    exit;
+}
+
+$q = "SELECT iUserID FROM user WHERE vMobile = '$phone' AND cStatus = 'A' LIMIT 1";
 $r = sql_query($q, 'LINK.APPLE.1');
 
 if (sql_num_rows($r)) {
     list($u_id) = sql_fetch_row($r);
-    sql_query(
-        "UPDATE user SET vAppleID = '" . db_input($apple_id) . "' 
-         WHERE iUserID = $u_id",
-        'LINK.APPLE.2'
-    );
+    sql_query("UPDATE user SET vAppleID = '$apple_id' WHERE iUserID = $u_id", 'LINK.APPLE.2');
 
-	echo json_encode([
-		"statusCode" => 200,
-		"data" => [
-			"message" => "Apple ID linked successfully"
-		]
-	]);
-	exit;
+    http_response_code(200);
+    echo json_encode([
+        "statusCode" => 200,
+        "data" => [
+            "message" => "Apple ID linked successfully"
+        ]
+    ]);
+    exit;
+
 } else {
-	http_response_code(201);
-	header('Content-Type: application/json');
-	echo json_encode([
-		"statusCode" => 201,
-		"error" => [
-			"message" => "No account found. Please register first."
-		]
-	]);
-	exit;
+    http_response_code(404);
+    echo json_encode([
+        "statusCode" => 404,
+        "error" => [
+            "message" => "No account found. Please register first."
+        ]
+    ]);
+    exit;
 }
