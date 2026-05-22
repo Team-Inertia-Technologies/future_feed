553157f6148cb2f81edac79976afce9bc66e0eea
Myron <myron@teaminertia.com>
2026-05-22 18:33:14 +0530

    modified
    Signed-off-by: Myron <myron@teaminertia.com>

--- public/api/for_you.php
+++ public/api/for_you.php
@@ -75,51 +75,7 @@ try {
         $tagConditions = " OR (" . implode(' OR ', $tagLikes) . ")";
     }
 
-    $videoQuery = "
-    SELECT 
-        v.*,
-        (
-            SELECT COUNT(*)
-            FROM user_liked_video ulv
-            WHERE ulv.iVideoID = v.iVideoID
-            AND ulv.cStatus = 'A'
-        ) AS like_count,
-
-        (
-            SELECT COUNT(*)
-            FROM comment c
-            WHERE c.iVideoID = v.iVideoID
-            AND c.cStatus = 'A'
-        ) AS comment_count,
-
-        EXISTS(
-            SELECT 1
-            FROM user_liked_video ul
-            WHERE ul.iVideoID = v.iVideoID
-            AND ul.iUserID = $userId
-            AND ul.cStatus = 'A'
-        ) AS isLiked
-
-    FROM videos v
-
-    WHERE v.cStatus = 'A'
-
-    AND NOT EXISTS (
-        SELECT 1
-        FROM user_watched_video uwv
-        WHERE uwv.iVideoID = v.iVideoID
-        AND uwv.iUserID = $userId
-        AND uwv.cStatus = 'A'
-    )
-
-    AND (
-        v.iFieldID IN ($fieldIdList)
-        $tagConditions
-    )
-
-    ORDER BY v.iVideoID DESC
-    LIMIT 10
-";
+   $videoQuery = " SELECT DISTINCT v.*, COUNT(DISTINCT vl.iAssocID) AS like_count, COUNT(DISTINCT vc.iCommentID) AS comment_count, CASE WHEN ul.iUserID IS NOT NULL THEN 1 ELSE 0 END AS isLiked FROM videos v LEFT JOIN user_watched_video uwv ON v.iVideoID = uwv.iVideoID AND uwv.iUserID = $userId AND uwv.cStatus = 'A' LEFT JOIN user_liked_video vl ON v.iVideoID = vl.iVideoID AND vl.cStatus = 'A' LEFT JOIN comment vc ON v.iVideoID = vc.iVideoID AND vc.cStatus = 'A' LEFT JOIN user_liked_video ul ON v.iVideoID = ul.iVideoID AND ul.iUserID = $userId AND ul.cStatus = 'A' WHERE v.cStatus = 'A' AND uwv.iVideoID IS NULL AND ( v.iFieldID IN ($fieldIdList) $tagConditions ) GROUP BY v.iVideoID ORDER BY RAND() LIMIT 10 ";
 
     $videoResult = sql_query($videoQuery);
 
